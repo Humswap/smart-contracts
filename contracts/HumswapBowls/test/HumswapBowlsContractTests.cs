61fe98d15753c7f83c748e13728a7e4f86d51ffe
jastez <jasontezan@gmail.com>
2021-10-03 16:14:20 -0700

    New Contracts

---
+++ contracts/HumswapBowls/test/HumswapBowlsContractTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using FluentAssertions;
+using Neo.Assertions;
+using Neo.BlockchainToolkit;
+using Neo.BlockchainToolkit.Models;
+using Neo.BlockchainToolkit.SmartContract;
+using Neo.SmartContract;
+using Neo.VM;
+using NeoTestHarness;
+using Xunit;
+
+namespace HumswapBowlsTests
+{
+    [CheckpointPath("test/bin/checkpoints/contract-deployed.neoxp-checkpoint")]
+    public class HumswapBowlsContractTests : IClassFixture<CheckpointFixture<HumswapBowlsContractTests>>
+    {
+        readonly CheckpointFixture fixture;
+        readonly ExpressChain chain;
+
+        public HumswapBowlsContractTests(CheckpointFixture<HumswapBowlsContractTests> fixture)
+        {
+            this.fixture = fixture;
+            this.chain = fixture.FindChain("HumswapBowlsTests.neo-express");
+        }
+    }
+}
